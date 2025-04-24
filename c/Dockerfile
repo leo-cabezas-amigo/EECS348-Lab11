a69d395afed75a00606017273ed2ad4beb1d6ccb
leo-cabezas-amigo <l859c248@ku.edu>
2025-04-23 20:56:56 -0500

    Added C program with Dockerfile

---
+++ c/Dockerfile
@@ -0,0 +1,4 @@
+FROM gcc:latest
+COPY hello_world.c .
+RUN gcc -o main hello_world.c
+CMD ["./main"]
---
+++ c/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    printf("Hello world! This is a Dockerized C program.\n");
+    return 0;
+}
